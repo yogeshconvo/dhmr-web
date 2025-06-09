c335dc8cce4830633c63b93692633128b27f235d
Omar Ragab <tryragab@gmail.com>
2025-06-09 12:33:21 +0300

    FIX FORM

--- src/sections/Admissions/HeroAdmissions.jsx
+++ src/sections/Admissions/HeroAdmissions.jsx
@@ -5,28 +5,29 @@ import BannerMobile from "../../assets/AdmissionBannerMobile.png";
 export default function EducationEnquiryPage() {
   const [height, setHeight] = useState("100vh");
   const [bgImage, setBgImage] = useState(Banner);
-  useEffect(() => {
-    var s = document.createElement("script");
-    if (s) {
-      s.type = "text/javascript";
-      s.async = true;
-      s.src = "https://widgets.in6.nopaperforms.com/emwgts.js";
-      document.body.appendChild(s);
-    }
-    console.log(s);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    // setTimeout(() => {
+    //   var s = document.createElement("script");
+    //   if (s) {
+    //     s.type = "text/javascript";
+    //     s.async = true;
+    //     s.src = "https://widgets.in6.nopaperforms.com/emwgts.js";
+    //     document.body.appendChild(s);
+    //   }
+    //   console.log(s);
+    //   setLoading(false);
+    // }, 2500);
     // Dynamically load the external script for the form widget
     // const scriptId = "nopaperforms-widget-script";
-
     // if (!document.getElementById(scriptId)) {
     //   const script = document.createElement("script");
     //   script.id = scriptId;
     //   script.type = "text/javascript";
     //   script.async = true;
     //   script.src = "https://widgets.in6.nopaperforms.com/emwgts.js";
-
     //   document.body.appendChild(script);
-
     //   script.onload = () => {
     //     console.log("NoPaperForms widget script loaded");
     //     // If the widget exposes a function to initialize after script load,
@@ -67,6 +68,11 @@ export default function EducationEnquiryPage() {
   }, []);
   return (
     <div className="relative w-full bg-white ">
+      <script
+        type="text/javascript"
+        async={true}
+        src="https://widgets.in6.nopaperforms.com/emwgts.js"
+      ></script>
       {/* Banner Section */}
       <div
         className="relative w-full bg-no-repeat bg- md:bg-cover admissions-slider-hight"
@@ -184,14 +190,37 @@ export default function EducationEnquiryPage() {
           <div className="bg-[#F04E30] text-white text-center py-4 lg:py-3 text-lg sm:text-2xl font-sans rounded-t-lg">
             ENQUIRE NOW
           </div>
-          <div className="w-full flex justify-center ">
-            <div
-              className="npf_wgts bg-white rounded shadow-lg "
-              data-height="512px"
-              data-w="0615c1812ea64fa9c301f9db4e34bd73"
-              style={{ width: 500, maxWidth: "100%" }}
-            ></div>
-          </div>
+          {!loading && (
+            <div className="w-full flex justify-center ">
+              <div
+                className="npf_wgts bg-white rounded shadow-lg "
+                data-height="512px"
+                data-w="0615c1812ea64fa9c301f9db4e34bd73"
+                style={{ width: 500, maxWidth: "100%" }}
+              ></div>
+            </div>
+          )}
+          {loading && (
+            <div className="p-18 flex items-center justify-center">
+              <svg
+                aria-hidden="true"
+                class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </div>
+          )}
         </div>
         {/* <p className="text-[10px] max-w-[90%] md:max-w-[70%] px-6 pb-3 sm:text-left font-oswald-light">
           *By submitting this form, I agree to receive notifications from the
